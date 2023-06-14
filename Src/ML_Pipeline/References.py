28c74c3b13495c91849334395415f64f03bb34ff
Megha Dey Sarkar <62908566+MeghaDeySarkar@users.noreply.github.com>
2023-06-14 16:27:25 +0800

    Add files via upload

---
+++ Src/ML_Pipeline/References.py
@@ -0,0 +1,10 @@
+
+
+class References:
+
+        path = 'D:\VSCode_Workspace\Image_Colorization\modular_code\Input'
+        ROOT_DIR=""
+        MODEL_NAME=""
+        SAVE_MODEL='D:/VSCode_Workspace/Image_Colorization/modular_code/Output/colorize_autoencoder_2000.model'
+        TEST_IMG="D:/VSCode_Workspace/Image_Colorization/modular_code/Output/generated_images"
+
